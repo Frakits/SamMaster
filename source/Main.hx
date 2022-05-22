3c04ecebbc9b6b6586b6021e96124b079e4f0054
Frakits <you@example.com>
2022-05-22 20:12:32 +0300

    super secret test (do not leak)

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,7 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
-import flixel.system.scaleModes.FillScaleMode;
+import flixel.system.scaleModes.FixedScaleAdjustSizeScaleMode;
 
 class Main extends Sprite
 {
@@ -85,7 +85,7 @@ class Main extends Sprite
 		FlxGraphic.defaultPersist = true;
 		// the reason for this is we're going to be handling our own cache smartly
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, false, startFullscreen));
-		FlxG.scaleMode = new FillScaleMode();
+		FlxG.scaleMode = new FixedScaleAdjustSizeScaleMode();
 		
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
