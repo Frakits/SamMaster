a014aa24ecc477489ffa4e84e0b5924d01c5214e
Frakits <you@example.com>
2022-05-22 12:13:57 +0300

    deeznutz

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -161,7 +161,6 @@ class LoadingState extends MusicBeatState
 
 		Paths.setCurrentLevel(directory);
 		trace('Setting asset folder to ' + directory);
-		trace("song:" + getSongPath());
 
 		#if NO_PRELOAD_ALL
 		var loaded:Bool = false;
